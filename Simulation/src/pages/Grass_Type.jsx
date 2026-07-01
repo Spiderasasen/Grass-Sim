aa4f950799a619e1e7b1faec49c8079572d4628b
spiderasasen <spiderasasen@gmail.com>
2026-07-01 09:53:57 -0400

    got the grass type page ready. now to finish the system then we can make it a bit more fancy if needed

--- Simulation/src/pages/Grass_Type.jsx
+++ Simulation/src/pages/Grass_Type.jsx
@@ -1,11 +1,35 @@
 import Header from "../conponents/Header.jsx";
 import Footer from "../conponents/Footer.jsx";
+import grassTypes from "../data/grass.json";
+import "../styles/grass-type-card.css";
 
 function Grass_Type() {
     return (
         <div>
             <Header/>
-            <h1>Grass Types</h1>
+
+            {/*main section*/}
+            <main>
+                <h1>Grass Types</h1>
+                <div className="grass-card-container">
+                    {grassTypes.grassTypes.map((grassType) => (
+                        <div className="grass-card" key={grassType.id}>
+                            <h2 className="grass-card-title">{grassType.name}</h2>
+                            <p className="grass-card-description">{grassType.description}</p>
+
+                            <div className="grass-card-info">
+                                <span className="grass-tag">{grassType.region}</span>
+                                <span className="grass-tag">{grassType.shadeTolerance} shade</span>
+                                <span className={`grass-tag ${grassType.category}`}>
+                    {grassType.category}
+                </span>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+
+            </main>
+
             <Footer/>
         </div>
     )
