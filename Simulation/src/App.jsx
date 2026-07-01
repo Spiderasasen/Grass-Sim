cb0a50a27adf6596f9b74142d67071cdad0f4e68
spiderasasen <spiderasasen@gmail.com>
2026-07-01 09:42:46 -0400

    got the other page to load

--- Simulation/src/App.jsx
+++ Simulation/src/App.jsx
@@ -1,12 +1,14 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import "./App.css"
+import Grass_Type from "./pages/Grass_Type.jsx";
 
 function App(){
   return(
       <Router>
         <Routes>
           <Route path="/" element={<Home/>}/>
+          <Route path="grass-types" element={<Grass_Type/>}/>
         </Routes>
       </Router>
   );
---
+++ Simulation/src/pages/Grass_Type.jsx
@@ -0,0 +1,13 @@
+import Header from "../conponents/Header.jsx";
+import Footer from "../conponents/Footer.jsx";
+
+function Grass_Type() {
+    return (
+        <div>
+            <Header/>
+            <h1>Grass Types</h1>
+            <Footer/>
+        </div>
+    )
+}
+export default Grass_Type;
